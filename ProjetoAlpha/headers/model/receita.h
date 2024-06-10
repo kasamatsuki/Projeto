ca60c80bd6abc07e6beb0331fcb9d89a98daa3b1
diogo <1230426@isep.ipp.pt>
2024-06-10 17:04:37 +0100

    Versao 0.1

--- ProjetoAlpha/headers/model/receita.h
+++ ProjetoAlpha/headers/model/receita.h
@@ -11,27 +11,41 @@ class Receita {
 private:
     string nomeReceita;
     string descricao;
+    bool fav;
     list<Ingrediente> ingredientes_receita;
+
 
 public:
     // Construtor
     Receita(const string& nomeReceita, const string& descricao, const list<Ingrediente>& ingredientes_receita);
 
+    // Construtor de cópia
+    Receita(const Receita& obj);
+
+    // Destrutor
+    ~Receita();
+
     // Getters
     string getNomeReceita() const;
     string getDescricao() const;
+    bool isFav() const;
     list<Ingrediente> getIngredientes() const;
 
     // Setters
     void setNomeReceita(const string& nomeReceita);
     void setDescricao(const string& descricao);
+    void setFav(bool fav);
     void setIngredientes(const list<Ingrediente>& ingredientes_receita);
 
     // Adicionar um ingrediente
-    void addIngrediente(const Ingrediente& ingredientes_receita);
+    void addIngrediente(const Ingrediente& ingrediente);
 
     // Remover um ingrediente
-    void removeIngrediente(const Ingrediente& ingredientes_receita);
+    void removeIngrediente(const Ingrediente& ingrediente);
+
+    // Operadores de igualdade
+    bool operator==(const Receita& obj) const;
+    bool operator==(const string& str) const;
 };
 
 #endif //PROJETOALPHA_RECEITA_H
