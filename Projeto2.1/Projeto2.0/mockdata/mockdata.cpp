27b6b360b129bfdf212bde2aca1b32457d7c9c27
Figueiras3 <161575928+Figueiras3@users.noreply.github.com>
2024-06-17 07:37:48 +0100

    Delete Projeto2.1 directory

--- Projeto2.1/Projeto2.0/mockdata/mockdata.cpp
+++
@@ -1,3 +0,0 @@
-//
-// Created by diogo on 16/06/2024.
-//
--- Projeto2.1/Projeto2.0/mockdata/mockdata.h
+++
@@ -1,8 +0,0 @@
-//
-// Created by diogo on 16/06/2024.
-//
-
-#ifndef MY_PROJECT_MOCKDATA_H
-#define MY_PROJECT_MOCKDATA_H
-
-#endif //MY_PROJECT_MOCKDATA_H
