392968b8d2722b0bfb25ae77253f9889446ab036
Figueiras3 <161575928+Figueiras3@users.noreply.github.com>
2024-06-17 10:36:45 +0100

    Delete Projeto2.0 directory

--- Projeto2.0/cmake-build-debug/googleTestLib/googletest-main/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/diogo/CLionProjects/Projeto2.0/googleTestLib/googletest-main
-# Build directory: C:/Users/diogo/CLionProjects/Projeto2.0/cmake-build-debug/googleTestLib/googletest-main
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
